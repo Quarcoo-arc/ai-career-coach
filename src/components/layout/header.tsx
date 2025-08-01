400f2d6f14617970317d00ff7d083a54b80bb355
Michael Quarcoo <michaelquarcoo04@gmail.com>
2025-08-01 18:28:38 +0000

    feat: add clerk config for user management

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import { Button } from "../ui/button";
 
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className="flex justify-end items-center p-4 gap-4 h-16">
+      <SignedOut>
+        <SignInButton>
+          <Button variant="ghost" className="rounded-full">
+            Sign In
+          </Button>
+        </SignInButton>
+        <SignUpButton>
+          <Button className="rounded-full">Sign Up</Button>
+        </SignUpButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  );
 };
 
 export default Header;
