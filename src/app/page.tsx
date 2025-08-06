6a3344d38b2c6d312fb4e6ffc04a3bd44d6d5e88
Michael Quarcoo <michaelquarcoo04@gmail.com>
2025-08-06 22:28:15 +0000

    feat: add user guide component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
-import { Features, HeroSection, Statistics } from "@/components/landing";
+import {
+  Features,
+  HeroSection,
+  Statistics,
+  UserGuide,
+} from "@/components/landing";
 
 export default function Home() {
   return (
@@ -7,6 +12,7 @@ export default function Home() {
       <HeroSection />
       <Features />
       <Statistics />
+      <UserGuide />
     </div>
   );
 }
--- src/components/landing/index.ts
+++ src/components/landing/index.ts
@@ -1,5 +1,6 @@
 import HeroSection from "./hero";
 import Features from "./features";
 import Statistics from "./statistics";
+import UserGuide from "./userGuide";
 
-export { HeroSection, Features, Statistics };
+export { HeroSection, Features, Statistics, UserGuide };
