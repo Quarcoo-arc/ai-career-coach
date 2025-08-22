ddb7c3cc860b49c89bb160a4c873203603b4b338
Michael Quarcoo <michaelquarcoo04@gmail.com>
2025-08-22 22:52:13 +0000

    feat: add onboarding form

---
+++ src/app/(main)/onboarding/_components/onboarding-form.tsx
@@ -0,0 +1,12 @@
+import { Industries } from "@/types";
+import React from "react";
+
+interface OnboardingFormProps {
+  industries: Industries;
+}
+
+const OnboardingForm = ({ industries }: OnboardingFormProps) => {
+  return <div>OnboardingForm</div>;
+};
+
+export default OnboardingForm;
